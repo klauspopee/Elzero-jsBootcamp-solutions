163b27eadde63f9078d5923af83742623dd837d6
Klaus <116884531+klauspopee@users.noreply.github.com>
2026-01-29 12:10:27 +0100

    updated

--- from33to37/Two/main.js
+++ from33to37/Two/main.js
@@ -1,6 +1,19 @@
 let num1 = 9;
-let str = "9";
-let str2 = "20";
+let str = 9;
+let str2 = 9;
+
+if (num1 == str) {
+    console.log(`num1 is the same value as str`)
+    if (num1 !== str) {
+        console.log(`num1 is the same value as str but not the same type`)
+    } 
+} 
+if (num1 != str2 && typeof(num1) != typeof(str2)) {
+    console.log(`num1 is not the same value or the same type as str2`)
+}
+if (typeof(str) == typeof(str2) && str != str2) {
+    console.log(`str is the same type as str2 but not the same value`)
+}
 
 // Output
 // "{num1} Is The Same Value As {str}"
@@ -8,15 +21,3 @@ let str2 = "20";
 // "{num1} Is Not The Same Value Or The Same Type As {str2}"
 // "{str} Is The Same Type As {str2} But Not The Same Value"
 
-if (num1 == str) {
-    console.log(`{num1} Is The Same Value As {str}`);
-}
-if (num1 !== str) {
-    console.log(`{num1} Is The Same Value As {str} But Not The Same Type`);
-}
-if (num1 != str2 && typeof num1 != typeof str2)  {
-    console.log(`{num1} Is Not The Same Value Or The Same Type As {str2}`);
-}
-if (typeof str == typeof str2 && str != str2){
-    console.log(`{str} Is The Same Type As {str2} But Not The Same Value`);
-}
