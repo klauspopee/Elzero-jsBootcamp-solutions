f5ce5da011dad2555767f7f126e7ff0531b0ed3f
unknown <saulgodman1999@gmail.com>
2026-06-11 15:36:44 +0100

    finished 21 from all course

---
+++ All-course/21/main.js
@@ -0,0 +1,11 @@
+function getLastDigit(num) {
+  // Your Code Here
+    return Number(num.toString().at(-1))
+}
+
+console.log(getLastDigit(1)); // 1
+console.log(getLastDigit(18)); // 8
+console.log(getLastDigit(305)); // 5
+console.log(getLastDigit(1569)); // 9
+console.log(typeof getLastDigit(1569)); // Number
+
