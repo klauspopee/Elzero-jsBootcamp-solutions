ab39c3d0abc534fb96b58417e6bbf6a970145ade
unknown <saulgodman1999@gmail.com>
2026-07-24 11:56:07 +0100

    22

--- All-course/22/main.js
+++ All-course/22/main.js
@@ -1,4 +1,4 @@
 let str1 = "AElzero";
 let str2 = "ZAcademy";
 
-console.log(`${str1.slice(1)} ${str2.slice(1)}`); // Elzero Academy
+console.log(`${str1.slice(-(-1))} ${str2.slice(-(-1))}`); // Elzero Academy
